255e6a400867247f2f0d0c66451ae7a88a208860
chuchro3 <chuchro3@illinois.edu>
2012-11-25 23:19:20 -0600

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chado::Application.config.secret_token = 'c577e180225621dd2e583db3487f51c443e10439b74f23834b862a2ed5f6b65957e97f5f45de91f374164a71f50072f43f765c5612024f0d4d08a7af2a70e636'
