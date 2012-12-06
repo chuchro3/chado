f1c5ed085134d5f1054f41ff4b2243e5da4e2025
Robert Chuchro <chuchro3@illinois.edu>
2012-12-06 17:58:10 -0600

    basic javascript working

---
+++ app/views/pages/home.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+	
+		alert("hi!");
+ // $('.pagelink').bind('ajax:complete', function() {
+  
+    $('#main').fadeOut(500, function() {
+    
+     // $('.display_groupons').html("<%= escape_javascript(render(:partial => 'searchresults')) %>");
+      
+     // $('.display_groupons').fadeIn(400);
+    });
+
+ // });
+
+
+
+});
